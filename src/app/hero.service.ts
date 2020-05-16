210f786ca829e7522c692bbf8aced25ab6633cd3
adityabandal <adityabandal007@gmail.com>
2020-05-16 23:35:12 +0530

    made heroes service asynchronous

--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -1,14 +1,14 @@
 import { Injectable } from '@angular/core';
 import { Hero } from './hero';
 import { HEROES } from './mock-heroes'
-import { from } from 'rxjs';
+import { Observable, of, from } from 'rxjs';
 @Injectable({
   providedIn: 'root'
 })
 export class HeroService {
 
   constructor() { }
-  getHeroes(): Hero[] {
-    return HEROES;
+  getHeroes(): Observable<Hero[]> {
+    return of(HEROES);
   }
 }
